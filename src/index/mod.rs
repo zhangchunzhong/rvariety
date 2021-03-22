be0db8f2267541faa43c07e44066b0ac9f27f26c
zhangchunzhong <zczxyz@126.com>
2021-03-22 14:50:23 +0800

    add download

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -3,8 +3,8 @@
 // Code is licensed under Apache License, Version 2.0.
 use anyhow::Result;
 
-static APP_USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36";
-static GET_URL_INTERVAL:u64 = 2;
+pub static APP_USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36";
+pub static GET_URL_INTERVAL:u64 = 2;
 
 trait Index {
     fn new(url: &'static str, pattern: &'static str, max_page: u32) -> Self;
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 //
 // Code is licensed under Apache License, Version 2.0.
 
+
 pub mod error;
 pub mod index;
+pub mod download;
